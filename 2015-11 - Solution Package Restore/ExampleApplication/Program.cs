56586e0d4f5e39316df5fe1dbab5b36b60262aff
Seravy <seravy@gmail.com>
2015-11-15 22:38:08 +0800

    Nov Blog update. Added nuget solution package restore.

---
+++ 2015-11 - Solution Package Restore/ExampleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
